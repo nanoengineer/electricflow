ea1603f1fc20543d7d1b76bd26797b95f2424018
Tony Wu <tonywu@Tonys-MacBook-Air.local>
2023-07-17 13:48:14 -0400

    Finger position maps to additional charges, palm orientation dictates charge value

--- sketch.js
+++ sketch.js
@@ -14,10 +14,14 @@ let sketch = function (p) {
     let leftHandColor = p.color("#4cbcf5");
     let rightHandColor = p.color("#f5823b");
 
-    let fieldDensity = 3.5;
+    let fieldDensity = 4;
     let fieldPoints = [];
     let charges = [];
 
+    const numOfAmbientCharges = 4;
+    const numOfFingerCharges = 5;
+
+    //For perlin noise
     let t = 0;
 
 
@@ -31,27 +35,13 @@ let sketch = function (p) {
             }
         }
 
-        charges.push(new Charge(width / 2, height / 2, 1));
-        charges.push(new Charge(width / 2, height / 2, -1));
-        charges.push(new Charge(width / 2, height / 2, 1));
-        charges.push(new Charge(width / 2, height / 2, -1));
-        charges.push(new Charge(width / 2, height / 2, 2));
-        charges.push(new Charge(width / 2, height / 2, -2));
-        // charges.push(new Charge(width / 2, height / 2, 60));
-        // charges.push(new Charge(width / 2, height / 2, -80));
-        // charges.push(new Charge(width / 2, height / 2, -40));
-
-
-
+        for (let i = 0; i < numOfAmbientCharges + numOfFingerCharges; i++) {
+            charges.push(new Charge(width / 2, height / 2, 1));
+        }
     };
 
     p.draw = function () {
         p.background(0);
-        // p.clear();
-        // p.noFill();
-        // p.stroke("#51b7cf");
-        // p.strokeWeight(5);
-        // p.rect(0, 0, width, height);
         let results = window.handDetectionResults;
 
         if (handResultValid(results)) {
@@ -65,9 +55,9 @@ let sketch = function (p) {
                     const lm = results.landmarks[(hand[0].index)];
                     const wlm = results.worldLandmarks[(hand[0].index)];
                     const palmFill = p.lerpColor(p.color("#FF0000"), p.color("#0000FF"), getPalmOrientation(wlm, hand[0].categoryName));
-                    drawPalmFill(lm, palmFill);
-                    drawHandConnections(lm);
-                    drawHandLandmarks(lm, dotColor);
+                    // drawPalmFill(lm, palmFill);
+                    // drawHandConnections(lm);
+                    // drawHandLandmarks(lm, dotColor);
                 }
             } else if (results.handednesses.length == 1) {
                 let hand = results.handednesses[0][0];
@@ -76,25 +66,27 @@ let sketch = function (p) {
                     dotColor = rightHandColor;
                 }
                 const lm = results.landmarks[0];
-                const palmFill = p.lerpColor(p.color("#FF0000"), p.color("#0000FF"), getPalmOrientation(results.worldLandmarks[0], hand.categoryName));
-                drawPalmFill(lm, palmFill);
-                drawHandConnections(lm);
-                drawHandLandmarks(lm, dotColor);
+                const palmOrient = getPalmOrientation(results.worldLandmarks[0], hand.categoryName)
+                const palmFill = p.lerpColor(p.color("#FF0000"), p.color("#0000FF"), palmOrient);
+                // drawPalmFill(lm, palmFill);
+                // drawHandConnections(lm);
+                // drawHandLandmarks(lm, dotColor);
 
-                // for (let i = 0; i < 5; i++) {
-                //     charges[i].position = p.createVector(lm[(i + 1) * 4].x * width, lm[(i + 1) * 4].y * height);
-                // }
-
+                setHandCharges(lm, p.map(palmOrient, 0, 1, -1, 1));
             }
         }
+        else {
+            clearHandCharges();
+        }
 
-        t = t + 0.005;
+        t = t + 0.004;
 
-
-        for (let i = 0; i < charges.length; i++) {
-            let x = p.noise(t + 5 + i) * 1.5 * width;
-            let y = p.noise(t + 80 + i) * 1.5 * height;
+        //Only evolve the ambient charges
+        for (let i = 0; i < numOfAmbientCharges; i++) {
+            let x = p.noise(t + 5 + i) * 2 * width - 0.5 * width;
+            let y = p.noise(t + 80 + i) * 2 * height - 0.5 * height;
             charges[i].position = p.createVector(x, y);
+            charges[i].charge = p.noise(t + 20 * i) * 8 - 4;
         }
 
         for (ch of charges) {
@@ -103,6 +95,8 @@ let sketch = function (p) {
 
         for (fp of fieldPoints) {
             fp.update(charges);
+            fp.negColor = p.lerpColor(p.color("#faab00"), p.color("#e35cf7"), p.noise(t));
+            fp.posColor = p.lerpColor(p.color("#009efa"), p.color("#6f00ff"), p.noise(t + 19));
             fp.draw();
         }
     };
@@ -114,49 +108,60 @@ let sketch = function (p) {
             this.pos = p.createVector(x, y);
             this.vector = p.createVector(0, 0);
             this.potential = 0;
-            this.negativeColor = p.color("#ff0000");
-            this.positiveColor = p.color("#0000f7");
+            this.negativeColor = p.color("#faab00");
+            this.positiveColor = p.color("#009efa");
         }
 
         update(charges) {
-            const field_potential = getElectricFieldandPotential(charges, this.pos);
+            const field_potential = this.#getElectricFieldandPotential(charges, this.pos);
             this.vector = field_potential.fieldVector;
             this.potential = field_potential.potential;
         }
 
         draw() {
-            let vectorDrawLength = p.constrain(this.vector.mag() * 10, 0, 25);
+            let vectorDrawLength = p.constrain(this.vector.mag() * 10, 0, 35);
             p.stroke(p.lerpColor(this.negativeColor, this.positiveColor, p.map(this.potential, -1000, 1000, 0, 1)));
-            p.strokeWeight(3);
+            p.strokeWeight(2);
             this.vector.normalize();
             p.line(this.pos.x, this.pos.y, this.pos.x - this.vector.x * vectorDrawLength, this.pos.y - this.vector.y * vectorDrawLength)
         }
-    }
 
-
-    function getElectricFieldandPotential(charges, point) {
-
-        const k = 100000;
-        let fieldVector = p.createVector(0, 0);
-        let potential = 0;
-
-        //electric potential kq/r
-        //electric field kq/r^2
-
-        for (let i = 0; i < charges.length; i++) {
-
-            const chPos = charges[i].position;
-            const chCharge = charges[i].chargeMag;
-            const distance = chPos.dist(point);
-            const vectorDirection = p.createVector(chPos.x - point.x, chPos.y - point.y).normalize();
-            const v = k * chCharge / (distance);
-            const fieldStrength = v / distance;
-            potential += v;
-            const E = vectorDirection.mult(fieldStrength);
-            fieldVector.add(E);
+        set negColor(nc) {
+            this.negativeColor = nc;
         }
-        return { fieldVector, potential };
+
+        set posColor(pc) {
+            this.positiveColor = pc;
+        }
+
+        #getElectricFieldandPotential(charges, point) {
+
+            const k = 100000;
+            let fieldVector = p.createVector(0, 0);
+            let potential = 0;
+
+            //electric potential kq/r
+            //electric field kq/r^2
+
+            for (let i = 0; i < charges.length; i++) {
+
+
+                const chCharge = charges[i].chargeMag;
+                if (chCharge == 0) {
+                    continue;
+                }
+                const chPos = charges[i].position;
+                const distance = chPos.dist(point);
+                const vectorDirection = p.createVector(chPos.x - point.x, chPos.y - point.y).normalize();
+                const v = k * chCharge / (distance);
+                const fieldStrength = v / distance;
+                potential += v;
+                fieldVector.add(vectorDirection.mult(fieldStrength));
+            }
+            return { fieldVector, potential };
+        }
     }
+
 
     class Charge {
 
@@ -181,6 +186,23 @@ let sketch = function (p) {
 
         set position(pos) {
             this.pos = pos;
+        }
+
+        // set charge(ch) {
+        //     this.charge = ch;
+        // }
+    }
+
+    function setHandCharges(lm, value) {
+        for (let i = numOfAmbientCharges; i < numOfAmbientCharges + numOfFingerCharges; i++) {
+            charges[i].position = p.createVector(lm[(i - numOfAmbientCharges + 1) * 4].x * width, lm[(i - numOfAmbientCharges + 1) * 4].y * height);
+            charges[i].charge = value;
+        }
+    }
+    function clearHandCharges() {
+        for (let i = numOfAmbientCharges; i < numOfAmbientCharges + numOfFingerCharges; i++) {
+            charges[i].position = p.createVector(0, 0);
+            charges[i].charge = 0;
         }
     }
 
